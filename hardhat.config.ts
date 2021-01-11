64007ff3c04f8ddf3eb0b5ad95fa5111ca639f99
James Zaki <james.zaki@gmail.com>
2021-01-11 16:00:47 +1100

    Use TypeScript for config and tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+import "@nomiclabs/hardhat-waffle";
+import { task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -13,10 +15,7 @@ task("accounts", "Prints the list of accounts", async () => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
@@ -29,3 +28,4 @@ module.exports = {
   }
 };
 
+export default config;
--- test/blsWallet-test.ts
+++ test/blsWallet-test.ts
@@ -1,6 +1,6 @@
-const { expect } = require("chai");
-const { BigNumber } = require("ethers");
-const { ethers } = require("hardhat");
+import { expect } from "chai";
+import { BigNumber, Signer } from "ethers";
+import { ethers } from "hardhat";
 const utils = ethers.utils;
 
 const acc1BLSPubKey = [0xAA, 0xBB, 0xCC, 0xDD].map((n) => { return BigNumber.from(n) });
@@ -11,15 +11,20 @@ const initialSupply = ethers.utils.parseUnits("1000000")
 const userStartAmount = initialSupply.div(2);
 
 describe('BLSWallet', async function () {
+  let admin: Signer, account1: Signer, account2: Signer;
+  let address1: string, address2: string;
+
   beforeEach(async function () {
     [admin, account1, account2] = await ethers.getSigners();
+    address1 = await account1.getAddress();
+    address2 = await account2.getAddress();
 
     // setup erc20 token and account balances
     const MockERC20 = await ethers.getContractFactory("MockERC20");
     this.baseToken = await MockERC20.deploy("AnyToken","TOK", initialSupply);
     await this.baseToken.deployed();
-    await this.baseToken.transfer(account1.address, userStartAmount);
-    await this.baseToken.transfer(account2.address, userStartAmount);
+    await this.baseToken.transfer(address1, userStartAmount);
+    await this.baseToken.transfer(address2, userStartAmount);
 
     // deploy bls wallet with token address
     const BLSWallet = await ethers.getContractFactory("MockBLSWallet");
@@ -33,22 +38,22 @@ describe('BLSWallet', async function () {
 
   it('should deposit balance from token to bls wallet', async function () {
     await this.blsWallet.connect(account1).deposit(acc1BLSPubKey, userStartAmount);
-    expect(await this.blsWallet.balanceOf(account1.address)).to.equal(userStartAmount);
+    expect(await this.blsWallet.balanceOf(address1)).to.equal(userStartAmount);
   });
 
   it('should set bls public key on deposit', async function () {
     await this.blsWallet.connect(account1).deposit(acc1BLSPubKey, userStartAmount);
-    expect(await this.blsWallet.blsPubKeyOf(account1.address)).to.eql(acc1BLSPubKey);
+    expect(await this.blsWallet.blsPubKeyOf(address1)).to.eql(acc1BLSPubKey);
   });
 
   it('should withdraw full balance from token to bls wallet', async function () {
     await this.blsWallet.connect(account1).withdraw();
-    expect(await this.blsWallet.balanceOf(account1.address)).to.equal(0);
+    expect(await this.blsWallet.balanceOf(address1)).to.equal(0);
   });
 
   it('should reset bls public key on withdraw', async function () {
     await this.blsWallet.connect(account1).withdraw();
-    expect(await this.blsWallet.blsPubKeyOf(account1.address)).to.eql(zeroBLSPubKey);
+    expect(await this.blsWallet.blsPubKeyOf(address1)).to.eql(zeroBLSPubKey);
   });
 
 });
