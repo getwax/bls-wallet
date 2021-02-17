4f142f2abca312b07b2d05799ebc803cd9e0a63f
James Zaki <james.zaki@gmail.com>
2021-02-17 15:24:08 +1100

    Convert contracts to use 0.7.* compiler

--- contracts/lib/BLS.sol
+++ contracts/lib/BLS.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.0;
 
 import { ModexpInverse, ModexpSqrt } from "./ModExp.sol";
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 require('dotenv').config();
+import { HardhatUserConfig } from "hardhat/types";
+
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+
 import { task } from "hardhat/config";
-import { HardhatUserConfig } from "hardhat/config";
-import { BigNumber, Signer, Contract } from "ethers";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,9 +19,9 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
-      {
-        version: "0.8.0"
-      },
+      // {
+      //   version: "0.8.0"
+      // },
       {
         version: "0.7.0"
       }
