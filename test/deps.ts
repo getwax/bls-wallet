d9c67271fa0339292e9742b17746ee820969c86b
Andrew Morris <voltrevo@gmail.com>
2021-04-19 16:41:32 +1000

    Migrate changes from https://github.com/jzaki/bls-wallet/pull/13

---
+++ test/deps.ts
@@ -0,0 +1,9 @@
+export {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.93.0/testing/asserts.ts";
+
+export { keccak256 } from "https://cdn.skypack.dev/@ethersproject/keccak256@v5.1.0?dts";
+export { formatBytes32String } from "https://cdn.skypack.dev/@ethersproject/strings@v5.1.0?dts";
+
+export { arrayify, hexlify } from "../src/deps.ts";
