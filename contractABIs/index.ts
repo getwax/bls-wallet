9d79c0800842b9512479699c87fe20ff24fa2a8d
Andrew Morris <voltrevo@gmail.com>
2021-06-01 07:09:50 +0000

    Fix contractABIs/index.ts

--- contractABIs/index.ts
+++ contractABIs/index.ts
@@ -2,7 +2,14 @@
 type ExplicitAny = any;
 
 async function readFileJson(path: string): Promise<ExplicitAny> {
-  return JSON.parse(new TextDecoder().decode(await Deno.readFile(path)));
+  const jsonString = new TextDecoder().decode(await Deno.readFile(path));
+
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    error.message = `${path}: ${error.message}`;
+    throw error;
+  }
 }
 
 const prefix = "file://";
@@ -16,7 +23,7 @@ const dir = import.meta.url.slice(prefix.length).replace(/\/[^\/]*$/, "");
 const abis: Record<string, ExplicitAny> = {};
 
 for await (const entry of Deno.readDir(dir)) {
-  if (entry.isFile && entry.name !== "mod.ts") {
+  if (entry.isFile && entry.name !== "index.ts") {
     abis[entry.name] = await readFileJson(`${dir}/${entry.name}`);
   }
 }
