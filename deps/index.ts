61742691bb7fb1c6fe282f300c9763c1b648f859
Andrew Morris <voltrevo@gmail.com>
2021-06-01 00:04:12 +0000

    Remove commented previous attempts

--- deps/index.ts
+++ deps/index.ts
@@ -25,28 +25,6 @@ export type Contract = ethers.Contract;
 export const Wallet = ethers.Wallet;
 export type Wallet = ethers.Wallet;
 
-// // // Ethers dependencies
-// export {
-//   // BigNumber,
-//   // Contract,
-//   default as ethers,
-//   // Wallet,
-// } from "./ethers/index.ts";
-// } from "https://unpkg.com/ethers@5.1.3/dist/ethers.esm.js?module";
-// } from "https://unpkg.com/ethers?module";
-// } from "https://cdn.skypack.dev/ethers"; // error: "hash.js" no dep found
-// } from "https://cdn.skypack.dev/ethers?min"; // error: reference in own type annotation
-// } from "https://cdn.skypack.dev/ethers@v5.1.3?dts";
-
-// import { Contract } from "https://cdn.skypack.dev/@ethersproject/contracts?dts";
-// import { Wallet } from "https://cdn.skypack.dev/@ethersproject/wallet?dts";
-
-// import dew from "https://dev.jspm.io/ethers";
-// const ContractClass = (dew as any).Contract;
-// export type Contract = typeof ContractClass;
-
-// import tsGenerator from 'https://cdn.skypack.dev/@typechain/ts-generator';
-
 // Database dependencies
 export {
   Constraint,
