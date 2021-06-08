ccebba52ea5301b33eff31c66b53b18a397e4ae1
Andrew Morris <voltrevo@gmail.com>
2021-06-08 04:42:53 +0000

    Fix app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,14 @@ import WalletService from "./WalletService.ts";
 import TxService from "./TxService.ts";
 import TxController from "./TxController.ts";
 import AdminController from "./AdminController.ts";
+import AdminService from "./AdminService.ts";
 
 const walletService = new WalletService();
 const txService = await TxService.create(env.TX_TABLE_NAME);
+const adminService = new AdminService(walletService, txService);
 
 const txController = new TxController(walletService, txService);
-const adminController = new AdminController(walletService, txService);
+const adminController = new AdminController(adminService);
 
 const app = createKoaApp({ adminController, txController });
 
