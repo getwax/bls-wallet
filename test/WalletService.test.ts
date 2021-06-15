cea59750b221337fc4300d3594875b8d6d60cb1d
Andrew Morris <voltrevo@gmail.com>
2021-06-15 02:29:10 +0000

    Add failing test

--- test/WalletService.test.ts
+++ test/WalletService.test.ts
@@ -2,6 +2,36 @@ import { assert } from "./deps.ts";
 
 import WalletService from "../src/app/WalletService.ts";
 
+// deno-lint-ignore no-explicit-any
+type ExplicitAny = any;
+
+function testWrapper(testFn: () => void | Promise<void>) {
+  return async () => {
+    try {
+      await testFn();
+    } catch (error) {
+      if (!(error.error instanceof Error)) {
+        throw error;
+      }
+
+      let innerError = error.error;
+
+      while (innerError.error instanceof Error) {
+        innerError = innerError.error;
+      }
+
+      const wrappedError = new Error(
+        `\n  innermost error: ${innerError.message}` +
+          `\n\n  error: ${error.message}`,
+      );
+
+      (wrappedError as ExplicitAny).error = error;
+
+      throw wrappedError;
+    }
+  };
+}
+
 Deno.test({
   name: "WalletService gets aggregator balance",
   sanitizeOps: false,
@@ -12,4 +42,14 @@ Deno.test({
       (await walletService.getAggregatorBalance()).gt(0),
     );
   },
+});
+
+Deno.test({
+  name: "WalletService sends empty aggregate transaction",
+  sanitizeOps: false,
+  fn: testWrapper(async () => {
+    const walletService = new WalletService();
+
+    await walletService.sendTxs([]);
+  }),
 });
