0bcb518050e3ae13604133d7ba9075528731e98b
Andrew Morris <voltrevo@gmail.com>
2021-06-16 07:05:34 +0000

    Test rejecting invalid transaction

--- test/TxService.test.ts
+++ test/TxService.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "./deps.ts";
 
 import Fixture from "./helpers/Fixture.ts";
 
-Fixture.test("adds transaction", async (fx) => {
+Fixture.test("adds valid transaction", async (fx) => {
   const txService = await fx.createTxService();
 
   const blsSigner = await fx.createBlsSigner();
@@ -17,6 +17,37 @@ Fixture.test("adds transaction", async (fx) => {
   });
 
   assertEquals(await txService.txTable.count(), 0n);
-  await txService.add(tx);
+
+  const failures = await txService.add(tx);
+  assertEquals(failures, []);
+
   assertEquals(await txService.txTable.count(), 1n);
+});
+
+Fixture.test("rejects invalid transaction", async (fx) => {
+  const txService = await fx.createTxService();
+
+  const blsSigner = await fx.createBlsSigner();
+  const blsWallet = await fx.getOrCreateBlsWallet(blsSigner);
+
+  const tx = await fx.createTxData({
+    blsSigner,
+    contract: fx.walletService.erc20,
+    method: "mint",
+    args: [blsWallet.address, "3"],
+    nonceOffset: 0,
+  });
+
+  tx.signature = [
+    "0x",
+    tx.signature[2] === "0" ? "1" : "0",
+    tx.signature.slice(3),
+  ].join("");
+
+  assertEquals(await txService.txTable.count(), 0n);
+
+  const failures = await txService.add(tx);
+  assertEquals(failures.length, 1);
+
+  assertEquals(await txService.txTable.count(), 0n);
 });
