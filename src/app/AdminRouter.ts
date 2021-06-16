e54945f4c2230bbd2b3df236ba08bb268c8a9b5f
Andrew Morris <voltrevo@gmail.com>
2021-06-16 05:22:59 +0000

    controllers -> routers

---
+++ src/app/AdminRouter.ts
@@ -0,0 +1,33 @@
+import { ethers, Router } from "../../deps/index.ts";
+
+import AdminService from "./AdminService.ts";
+
+export default function AdminRouter(adminService: AdminService) {
+  const router = new Router({ prefix: "/admin/" });
+
+  router.get("countTxs", async (ctx) => {
+    const c = await adminService.txCount();
+    console.log(`Returning count ${c}\n`);
+    ctx.response.headers.set("Content-Type", "application/json");
+    ctx.response.body = c;
+  });
+
+  router.get("resetTxs", async (ctx) => {
+    await adminService.resetTxs();
+    ctx.response.body = "Transactions reset";
+  });
+
+  router.get("sendBatch", async (ctx) => {
+    await adminService.sendBatch();
+    ctx.response.body = "Sent batch of transactions";
+  });
+
+  router.get("aggregatorBalance", async (ctx) => {
+    ctx.response.body = ethers.utils.formatUnits(
+      (await adminService.getAggregatorBalance()).toString(),
+      18,
+    );
+  });
+
+  return router;
+}
---
+++ src/app/TxRouter.ts
@@ -0,0 +1,19 @@
+import { Router } from "../../deps/index.ts";
+
+import TxService from "./TxService.ts";
+import type { TransactionData } from "./TxTable.ts";
+
+export default function TxRouter(txService: TxService) {
+  const router = new Router();
+
+  router.post("transaction", async (ctx) => {
+    const txData: TransactionData = await (await ctx.request.body()).value;
+    await txService.add(txData);
+
+    //TODO: send tx(s) after batch count, or N ms since last send.
+
+    ctx.response.body = "Transaction added";
+  });
+
+  return router;
+}
