b9b7402dc19a63a7f290796ddbb2f8dcaad6175e
Andrew Morris <voltrevo@gmail.com>
2021-06-16 07:29:22 +0000

    Add test for multiple failures

--- test/TxService.test.ts
+++ test/TxService.test.ts
@@ -80,3 +80,44 @@ Fixture.test("rejects transaction with nonce from the past", async (fx) => {
   // Transaction table remains empty
   assertEquals(await txService.txTable.count(), 0n);
 });
+
+Fixture.test(
+  "rejects transaction with invalid signature and nonce from the past",
+  async (fx) => {
+    const txService = await fx.createTxService();
+
+    const blsSigner = await fx.createBlsSigner();
+    const blsWallet = await fx.getOrCreateBlsWallet(blsSigner);
+
+    const tx = await fx.createTxData({
+      blsSigner,
+      contract: fx.walletService.erc20,
+      method: "mint",
+      args: [blsWallet.address, "3"],
+      nonceOffset: -1,
+    });
+
+    // createTxData would have correctly set nonce 1 if we hadn't used offset -1.
+    // (a transaction with nonce 0 occurs when creating the wallet)
+    assertEquals(tx.nonce, 0);
+
+    // Make the signature invalid
+    tx.signature = [
+      "0x",
+      tx.signature[2] === "0" ? "1" : "0",
+      tx.signature.slice(3),
+    ].join("");
+
+    assertEquals(await txService.txTable.count(), 0n);
+
+    const failures = await txService.add(tx);
+
+    assertEquals(
+      failures.map((f) => f.type).sort(),
+      ["duplicate-nonce", "invalid-signature"],
+    );
+
+    // Transaction table remains empty
+    assertEquals(await txService.txTable.count(), 0n);
+  },
+);
