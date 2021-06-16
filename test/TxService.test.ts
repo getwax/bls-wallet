5d16d2a8895be9e3ecfd13051fc7f3f07a803605
Andrew Morris <voltrevo@gmail.com>
2021-06-16 07:17:37 +0000

    Test rejection for duplicate nonce

--- test/TxService.test.ts
+++ test/TxService.test.ts
@@ -53,3 +53,30 @@ Fixture.test("rejects transaction with invalid signature", async (fx) => {
   // Transaction table remains empty
   assertEquals(await txService.txTable.count(), 0n);
 });
+
+Fixture.test("rejects transaction with nonce from the past", async (fx) => {
+  const txService = await fx.createTxService();
+
+  const blsSigner = await fx.createBlsSigner();
+  const blsWallet = await fx.getOrCreateBlsWallet(blsSigner);
+
+  const tx = await fx.createTxData({
+    blsSigner,
+    contract: fx.walletService.erc20,
+    method: "mint",
+    args: [blsWallet.address, "3"],
+    nonceOffset: -1,
+  });
+
+  // createTxData would have correctly set nonce 1 if we hadn't used offset -1.
+  // (a transaction with nonce 0 occurs when creating the wallet)
+  assertEquals(tx.nonce, 0);
+
+  assertEquals(await txService.txTable.count(), 0n);
+
+  const failures = await txService.add(tx);
+  assertEquals(failures.map((f) => f.type), ["duplicate-nonce"]);
+
+  // Transaction table remains empty
+  assertEquals(await txService.txTable.count(), 0n);
+});
