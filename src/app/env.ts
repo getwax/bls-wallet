2d6ea5664c4ae02e99be1ee33d45803315a406b4
Andrew Morris <voltrevo@gmail.com>
2021-07-13 06:40:29 +0000

    Add comment for PENDING_BATCH_SIZE

--- src/app/env.ts
+++ src/app/env.ts
@@ -30,7 +30,10 @@ export const PG = {
 
 export const TX_TABLE_NAME = requireEnv("TX_TABLE_NAME");
 export const PENDING_TX_TABLE_NAME = requireEnv("PENDING_TX_TABLE_NAME");
+
+/** Query size used when moving txs from pending table into main table. */
 export const PENDING_BATCH_SIZE = requireIntEnv("PENDING_BATCH_SIZE");
+
 export const MAX_PENDING_TXS = requireIntEnv("MAX_PENDING_TXS");
 
 export const LOG_QUERIES = requireBoolEnv("LOG_QUERIES");
