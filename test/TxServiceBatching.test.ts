8db469c02345167d0f53a5aa7e9a7b7165998688
Andrew Morris <voltrevo@gmail.com>
2021-07-21 00:32:53 +0000

    Add full batch test

--- test/TxServiceBatching.test.ts
+++ test/TxServiceBatching.test.ts
@@ -1,8 +1,19 @@
+import TxService from "../src/app/TxService.ts";
 import { assertEquals, ethers } from "./deps.ts";
 import Fixture from "./helpers/Fixture.ts";
+import Range from "./helpers/Range.ts";
+
+const txServiceConfig = {
+  ...TxService.defaultConfig,
+
+  // These may be the defaults, but they're technically env dependent, so we
+  // make sure we have these values because the tests assume them.
+  maxAggregationSize: 5,
+  maxAggregationDelayMillis: 5000,
+};
 
 Fixture.test("submit a single transaction in a timed batch", async (fx) => {
-  const txService = await fx.createTxService();
+  const txService = await fx.createTxService(txServiceConfig);
   const [{ blsSigner, blsWallet }] = await fx.setupWallets(1);
 
   const tx = await fx.createTxData({
@@ -37,6 +48,37 @@ Fixture.test("submit a single transaction in a timed batch", async (fx) => {
     ready: [],
     future: [],
   });
+});
+
+Fixture.test("submit a full batch without delay", async (fx) => {
+  const txService = await fx.createTxService();
+  const [{ blsSigner, blsWallet }] = await fx.setupWallets(1);
+
+  const firstBatchPromise = txService.batchTimer.waitForNextCompletion();
+
+  const txs = await Promise.all(
+    Range(5).map((i) =>
+      fx.createTxData({
+        blsSigner,
+        contract: fx.walletService.erc20,
+        method: "mint",
+        args: [blsWallet.address, "1"],
+        nonceOffset: i,
+      })
+    ),
+  );
+
+  const failures = await Promise.all(txs.map((tx) => txService.add(tx)));
+  assertEquals(failures.flat(), []);
+
+  await firstBatchPromise;
+
+  // Check mints have occurred, ensuring a batch has occurred even though the
+  // clock has not advanced
+  assertEquals(
+    await fx.walletService.getBalanceOf(blsWallet.address),
+    ethers.BigNumber.from(1005), // 1000 (initial) + 5 * 1 (mint txs)
+  );
 });
 
 // TODO: More tests
