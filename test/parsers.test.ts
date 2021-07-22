15b87b6a41d4542703c69e6c786ca69435690eda
Andrew Morris <voltrevo@gmail.com>
2021-07-22 07:14:03 +0000

    Fix test

--- test/parsers.test.ts
+++ test/parsers.test.ts
@@ -20,16 +20,24 @@ Deno.test("parseTransactionData reports missing fields for undefined", () => {
 });
 
 Deno.test("parseTransactionData accepts dummy values", () => {
-  // TODO: Be more strict, e.g. correctly formatted address
-
   const dummyTxData: TransactionData = {
-    pubKey: "pubKey",
-    nonce: 1,
-    signature: "signature",
-    tokenRewardAmount: "1",
-    contractAddress: "contractAddress",
-    methodId: "methodId",
-    encodedParams: "encodedParams",
+    "pubKey": [
+      "0x000102030405060708091011121314151617181920212223242526272829303132333",
+      "43536373839404142434445464748495051525354555657585960616263646566676869",
+      "70717273747576777879808182838485868788899091929394959697989900010203040",
+      "506070809101112131415161718192021222324252627",
+    ].join(""),
+    "nonce": 1,
+    "signature": [
+      "0x000102030405060708091011121314151617181920212223242526272829303132333",
+      "43536373839404142434445464748495051525354555657585960616263",
+    ].join(""),
+    "tokenRewardAmount": [
+      "0x0001020304050607080910111213141516171819202122232425262728293031",
+    ].join(""),
+    "contractAddress": "0x0001020304050607080910111213141516171819",
+    "methodId": "0x00010203",
+    "encodedParams": "0x00010203040506",
   };
 
   assertEquals(
