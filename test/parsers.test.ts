4df98f407206c843461c4f932da9f8da39657e9b
Andrew Morris <voltrevo@gmail.com>
2021-07-22 05:26:48 +0000

    Add test

--- test/parsers.test.ts
+++ test/parsers.test.ts
@@ -1,4 +1,5 @@
 import { parseTransactionData } from "../src/app/parsers.ts";
+import { TransactionData } from "../src/app/TxTable.ts";
 import { assertEquals } from "./deps.ts";
 
 Deno.test("parseTransactionData reports missing fields for undefined", () => {
@@ -15,5 +16,24 @@ Deno.test("parseTransactionData reports missing fields for undefined", () => {
         "field encodedParams: not provided",
       ],
     },
+  );
+});
+
+Deno.test("parseTransactionData accepts dummy values", () => {
+  // TODO: Be more strict, e.g. correctly formatted address
+
+  const dummyTxData: TransactionData = {
+    pubKey: "pubKey",
+    nonce: 1,
+    signature: "signature",
+    tokenRewardAmount: "1",
+    contractAddress: "contractAddress",
+    methodId: "methodId",
+    encodedParams: "encodedParams",
+  };
+
+  assertEquals(
+    parseTransactionData(dummyTxData),
+    { success: dummyTxData },
   );
 });
