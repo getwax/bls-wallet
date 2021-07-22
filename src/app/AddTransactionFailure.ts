f477809538e060b5773f754a4b5df280e31a040b
Andrew Morris <voltrevo@gmail.com>
2021-07-22 05:38:31 +0000

    Use parseTransactionData in TxRouter

--- src/app/AddTransactionFailure.ts
+++ src/app/AddTransactionFailure.ts
@@ -1,4 +1,5 @@
 type AddTransactionFailure = (
+  | { type: "invalid-format"; description: string }
   | { type: "invalid-signature"; description: string }
   | { type: "duplicate-nonce"; description: string }
   | { type: "insufficient-reward"; description: string }
--- src/app/TxRouter.ts
+++ src/app/TxRouter.ts
@@ -1,20 +1,43 @@
-import { HTTPStatus, Router } from "../../deps/index.ts";
+import { HTTPStatus, Router, RouterContext } from "../../deps/index.ts";
+import assert from "../helpers/assert.ts";
+import AddTransactionFailure from "./AddTransactionFailure.ts";
+import { parseTransactionData } from "./parsers.ts";
 
 import TxService from "./TxService.ts";
-import type { TransactionData } from "./TxTable.ts";
+
+function fail(ctx: RouterContext, failures: AddTransactionFailure[]) {
+  assert(failures.length > 0);
+
+  ctx.response.status = HTTPStatus.BadRequest;
+  ctx.response.body = { failures };
+}
 
 export default function TxRouter(txService: TxService) {
   const router = new Router({ prefix: "/" });
 
   router.post("transaction", async (ctx) => {
-    const txData: TransactionData = await (await ctx.request.body()).value;
+    const body: unknown = await (await ctx.request.body()).value;
+
+    const parsedBody = parseTransactionData(body);
+
+    if ("failures" in parsedBody) {
+      return fail(
+        ctx,
+        parsedBody.failures.map(
+          (description) => ({ type: "invalid-format", description }),
+        ),
+      );
+    }
+
+    const txData = parsedBody.success;
+
     const failures = await txService.add(txData);
 
     if (failures.length > 0) {
-      ctx.response.status = HTTPStatus.BadRequest;
+      return fail(ctx, failures);
     }
 
-    ctx.response.body = { failures };
+    ctx.response.body = { failures: [] };
   });
 
   return router;
