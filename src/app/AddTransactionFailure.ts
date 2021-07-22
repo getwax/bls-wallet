dc31880db080e87c5dd4afe093d89e31788aeef4
Andrew Morris <voltrevo@gmail.com>
2021-07-22 06:37:58 +0000

    Handle UNPREDICTABLE_GAS_LIMIT

--- src/app/AddTransactionFailure.ts
+++ src/app/AddTransactionFailure.ts
@@ -3,6 +3,7 @@ type AddTransactionFailure = (
   | { type: "invalid-signature"; description: string }
   | { type: "duplicate-nonce"; description: string }
   | { type: "insufficient-reward"; description: string }
+  | { type: "unpredictable-gas-limit"; description: string }
 );
 
 export default AddTransactionFailure;
