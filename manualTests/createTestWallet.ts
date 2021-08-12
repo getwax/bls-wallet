b16b5ad41e08dad44484241c5a4761b76e12d712
Andrew Morris <voltrevo@gmail.com>
2021-08-12 07:59:12 +0000

    Fix missing rpc url in createTestWallet

--- manualTests/createTestWallet.ts
+++ manualTests/createTestWallet.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env -S deno run --allow-net --allow-env --allow-read --allow-write --unstable
 
 import { ethers } from "../deps/index.ts";
+
+import * as env from "../src/env.ts";
 import TestBlsWallets from "./helpers/TestBlsWallets.ts";
 
 const [wallet] = await TestBlsWallets(
-  new ethers.providers.JsonRpcProvider(),
+  new ethers.providers.JsonRpcProvider(env.RPC_URL),
   1,
 );
 
