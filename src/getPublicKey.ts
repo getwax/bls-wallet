73d724720a8e58ffe35b021419e9446121b4e919
Andrew Morris <voltrevo@gmail.com>
2021-09-07 01:22:19 +0000

    Use BlsSignerFactory to help ease transition to future versions

--- src/getPublicKey.ts
+++ src/getPublicKey.ts
@@ -1,14 +1,12 @@
 import * as hubbleBls from "../deps/hubble-bls";
 
 export default (
+  signerFactory: hubbleBls.signer.BlsSignerFactory,
   domain: Uint8Array
 ) => (
   privateKey: string
 ): string => {
-  const signer = new hubbleBls.signer.BlsSigner(
-    domain,
-    hubbleBls.mcl.parseFr(privateKey),
-  );
+  const signer = signerFactory.getSigner(domain, privateKey);
 
   return hubbleBls.mcl.dumpG2(signer.pubkey);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { mcl } from "../deps/hubble-bls";
+import { signer } from "../deps/hubble-bls";
 
 import aggregate from "./aggregate";
 import defaultDomain from "./defaultDomain";
@@ -20,13 +20,19 @@ export async function initBlsWalletSigner({
   domain?: Uint8Array;
   chainId: number;
 }) {
-  await mcl.init();
+  // Note: Getting signers via this factory ensures that mcl-wasm's underlying
+  // init() has been called when signing. However, other operations such as
+  // signature verification probably also require this initialization, but have
+  // no similar factory. By guarding access to bls-wallet-signer's apis behind
+  // the creation of the signer factory, we're ensuring that mcl-wasm is
+  // properly initialized for all use cases, not just signing.
+  const signerFactory = await signer.BlsSignerFactory.new();
 
   return {
     aggregate,
-    getPublicKey: getPublicKey(domain),
+    getPublicKey: getPublicKey(signerFactory, domain),
     getPublicKeyHash: getPublicKeyHash(domain),
-    sign: sign(domain, chainId),
+    sign: sign(signerFactory, domain, chainId),
     verify: verify(domain, chainId),
     verifyAggregate: verifyAggregate(domain, chainId),
   };
