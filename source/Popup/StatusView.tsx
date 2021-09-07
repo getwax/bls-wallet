5c2c702c55fe640dcf35a4e94d853170bea95215
Andrew Morris <voltrevo@gmail.com>
2021-09-07 18:19:34 +1000

    Simplistic key creation

--- source/Popup/StatusView.tsx
+++ source/Popup/StatusView.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 
 import { BlsWalletSigner } from 'bls-wallet-signer';
+import assert from '../helpers/assert';
+import Range from '../helpers/Range';
 
 type Props = {
   blsWalletSigner: BlsWalletSigner;
 };
 
 type State = {
-  privateKey?: string;
-  walletAddress?: string;
+  wallet?: {
+    privateKey: string;
+    address?: string;
+  };
 };
 
 export default class StatusView extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
   render(): React.ReactNode {
     return (
       <div className="status-view">
@@ -20,7 +29,7 @@ export default class StatusView extends React.Component<Props, State> {
           <table className="basic-form">
             <tr>
               <td>BLS Key</td>
-              <td>key...</td>
+              <td>{this.renderKeyField()}</td>
             </tr>
             <tr>
               <td>BLS Wallet</td>
@@ -31,4 +40,59 @@ export default class StatusView extends React.Component<Props, State> {
       </div>
     );
   }
+
+  renderKeyField(): React.ReactNode {
+    const publicKey = this.PublicKey();
+
+    if (publicKey === undefined) {
+      return (
+        <>
+          <button type="button" onClick={() => this.createKey()}>
+            Create
+          </button>
+          <button type="button">Restore</button>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <span>
+          {publicKey.slice(0, 6)}...{publicKey.slice(-4)}
+        </span>
+        <button type="button">Delete</button>
+      </>
+    );
+  }
+
+  createKey(): void {
+    this.setState({
+      wallet: {
+        privateKey: generateRandomHex(256),
+      },
+    });
+  }
+
+  PublicKey(): string | undefined {
+    const { privateKey } = this.state.wallet ?? {};
+
+    if (privateKey === undefined) {
+      return undefined;
+    }
+
+    return this.props.blsWalletSigner.getPublicKey(privateKey);
+  }
+}
+
+function generateRandomHex(bits: number) {
+  const bytes = bits / 8;
+  assert(bytes === Math.round(bytes));
+
+  const hexBytes = Range(bytes).map(() =>
+    Math.floor(256 * Math.random())
+      .toString(16)
+      .padStart(2, '0')
+  );
+
+  return `0x${hexBytes.join('')}`;
 }
