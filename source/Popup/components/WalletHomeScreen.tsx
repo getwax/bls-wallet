68337f8b371ca517af23c5b1c24309de86e9174e
Andrew Morris <voltrevo@gmail.com>
2021-09-20 17:55:41 +1000

    Working on field-list

--- source/Popup/components/WalletHomeScreen.tsx
+++ source/Popup/components/WalletHomeScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { browser } from 'webextension-polyfill-ts';
 
 import CompactQuillHeading from './CompactQuillHeading';
 
@@ -8,7 +9,59 @@ const WalletHomeScreen = (): React.ReactElement => (
       <CompactQuillHeading />
     </div>
     <div className="section">
-      <div className="field-list">BLS Key:</div>
+      <div className="field-list">
+        <div>
+          <div style={{ width: '17px' }}>
+            <img
+              src={browser.runtime.getURL('assets/key.svg')}
+              alt="key"
+              width="14"
+              height="15"
+            />
+          </div>
+          <div className="field-label">BLS Key:</div>
+          <div className="field-value grow">(value)</div>
+          <div className="field-trailer">
+            <img
+              src={browser.runtime.getURL('assets/download.svg')}
+              alt="download"
+              width="22"
+              height="22"
+            />
+            <img
+              src={browser.runtime.getURL('assets/trashcan.svg')}
+              alt="delete"
+              width="22"
+              height="22"
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ width: '17px' }}>
+            <img
+              src={browser.runtime.getURL('assets/network.svg')}
+              alt="network"
+              width="14"
+              height="15"
+            />
+          </div>
+          <div className="field-label">Network:</div>
+          <div className="field-value">(dropdown)</div>
+        </div>
+        <div>
+          <div style={{ width: '17px' }}>
+            <img
+              src={browser.runtime.getURL('assets/address.svg')}
+              alt="address"
+              width="14"
+              height="15"
+            />
+          </div>
+          <div className="field-label">Address:</div>
+          <div className="field-value grow">(value)</div>
+          <div className="field-trailer">#86755</div>
+        </div>
+      </div>
     </div>
   </div>
 );
