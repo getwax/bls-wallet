0a4ee2eddac63d4fc9477bdbabe848ad8a4e546f
Andrew Morris <voltrevo@gmail.com>
2021-09-20 13:19:35 +1000

    Fix logo

--- source/Popup/FigmaTesting.tsx
+++ source/Popup/FigmaTesting.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
+import { browser } from 'webextension-polyfill-ts';
 
 import './figmaTesting.scss';
 
 const FigmaTesting = (): React.ReactElement => (
-  <div
-    style={{
-      minWidth: '359px',
-      maxWidth: '359px',
-      height: '500px',
-    }}
-  >
-    <div className="e8_189">
-      <div className="e8_190">
-        <span className="ei8_190_1_513">Quill</span>
-        <div className="ei8_190_1_515" />
+  <div className="popup">
+    <div className="container">
+      <div className="logo">
+        <img
+          src={browser.runtime.getURL('assets/logo.svg')}
+          alt="Quill"
+          width="80"
+          height="56"
+        />
       </div>
       <div className="e8_193">
         <span className="e8_194">Create BLS Key</span>
