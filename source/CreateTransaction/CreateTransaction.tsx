9924bd05b0733232ebe8d96cfca3549a2f59fa56
Andrew Morris <voltrevo@gmail.com>
2021-10-06 15:14:08 +1100

    Convert to component

--- source/CreateTransaction/CreateTransaction.tsx
+++ source/CreateTransaction/CreateTransaction.tsx
@@ -6,67 +6,67 @@ import QuillEthereumProvider from '../PageContentScript/QuillEthereumProvider';
 
 type Props = { events: PageEvents };
 
-const CreateTransaction: React.FC<Props> = (props: Props) => {
-  return (
-    <div className="create-transaction">
-      <div className="section">
-        <h1>Create Transaction</h1>
-      </div>
-      <div className="section body">
-        <div className="form">
-          <div>
-            Amount: <input type="text" style={{ textAlign: 'end' }} /> ETH
-          </div>
-          <div>
-            Recipient: <input type="text" />
-          </div>
-          <div>
-            <div style={{ display: 'inline-block' }}>
-              <Button
-                highlight={true}
-                onPress={async () => {
-                  const provider = getPropOrUndefined(window, 'ethereum') as
-                    | QuillEthereumProvider
-                    | undefined;
+export default class CreateTransaction extends React.Component<Props> {
+  render(): React.ReactElement {
+    return (
+      <div className="create-transaction">
+        <div className="section">
+          <h1>Create Transaction</h1>
+        </div>
+        <div className="section body">
+          <div className="form">
+            <div>
+              Amount: <input type="text" style={{ textAlign: 'end' }} /> ETH
+            </div>
+            <div>
+              Recipient: <input type="text" />
+            </div>
+            <div>
+              <div style={{ display: 'inline-block' }}>
+                <Button
+                  highlight={true}
+                  onPress={async () => {
+                    const provider = getPropOrUndefined(window, 'ethereum') as
+                      | QuillEthereumProvider
+                      | undefined;
 
-                  if (provider === undefined) {
-                    props.events.emit(
-                      'notification',
-                      'error',
-                      'Ethereum provider not found',
-                    );
+                    if (provider === undefined) {
+                      this.props.events.emit(
+                        'notification',
+                        'error',
+                        'Ethereum provider not found',
+                      );
 
-                    return;
-                  }
+                      return;
+                    }
 
-                  try {
-                    const result = await provider.request({
-                      method: 'eth_sendTransaction',
-                      params: [{}],
-                    });
+                    try {
+                      const result = await provider.request({
+                        method: 'eth_sendTransaction',
+                        params: [{}],
+                      });
 
-                    props.events.emit(
-                      'notification',
-                      'info',
-                      `Result: ${result}`,
-                    );
-                  } catch (error) {
-                    props.events.emit(
-                      'notification',
-                      'error',
-                      (error as Error).message,
-                    );
-                  }
-                }}
-              >
-                Send
-              </Button>
+                      this.props.events.emit(
+                        'notification',
+                        'info',
+                        `Result: ${result}`,
+                      );
+                    } catch (error) {
+                      this.props.events.emit(
+                        'notification',
+                        'error',
+                        (error as Error).message,
+                      );
+                    }
+                  }}
+                >
+                  Send
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default CreateTransaction;
+    );
+  }
+}
