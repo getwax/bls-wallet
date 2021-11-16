54fe9d5303a89d845326d9ee77b92cf096ecd0d3
Andrew Morris <voltrevo@gmail.com>
2021-11-16 03:47:18 +0000

    Update signer for executeActions

--- signer/src/types.ts
+++ signer/src/types.ts
@@ -1,10 +1,15 @@
 import { BigNumber } from "@ethersproject/bignumber";
 
-export type TransactionTemplate = {
-  nonce: BigNumber;
+export type ActionData = {
   ethValue: BigNumber;
   contractAddress: string;
   encodedFunction: string;
+};
+
+export type TransactionTemplate = {
+  nonce: BigNumber;
+  atomic: boolean;
+  actions: ActionData[];
 };
 
 export type SubTransaction = TransactionTemplate & { publicKey: string };
