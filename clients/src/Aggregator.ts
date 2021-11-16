c3d7e3013f2e5f45014d611723e2d4343c55cfbe
Andrew Morris <voltrevo@gmail.com>
2021-11-16 05:13:39 +0000

    Update Aggregator client interface for executeActions, remove obsoleted createWallet

--- clients/src/Aggregator.ts
+++ clients/src/Aggregator.ts
@@ -1,5 +1,4 @@
-import { TransactionData } from 'bls-wallet-signer';
-import { BigNumber } from 'ethers';
+import { Transaction } from 'bls-wallet-signer';
 
 type TransactionFailure =
   | { type: 'invalid-format'; description: string }
@@ -9,10 +8,22 @@ type TransactionFailure =
   | { type: 'unpredictable-gas-limit'; description: string }
   | { type: 'invalid-creation'; description: string };
 
-type TransactionDataDTO = {
-  [K in keyof TransactionData]: TransactionData[K] extends BigNumber
-    ? string
-    : TransactionData[K];
+export type ActionDataDTO = {
+  ethValue: string;
+  contractAddress: string;
+  encodedFunction: string;
+};
+
+export type SubTransactionDTO = {
+  publicKey: string;
+  nonce: string;
+  atomic: boolean;
+  actions: ActionDataDTO[];
+};
+
+export type TransactionDTO = {
+  subTransactions: SubTransactionDTO[];
+  signature: string;
 };
 
 type CreateWalletResult = {
@@ -33,7 +44,7 @@ export default class Aggregator {
     this.origin = new URL(url).origin;
   }
 
-  async addTransaction(tx: TransactionData): Promise<TransactionFailure[]> {
+  async addTransaction(tx: Transaction): Promise<TransactionFailure[]> {
     const resp = await fetch(`${this.origin}/transaction`, {
       method: 'POST',
       body: JSON.stringify(toDto(tx)),
@@ -58,38 +69,20 @@ export default class Aggregator {
 
     return json.failures;
   }
-
-  async createWallet(tx: TransactionData): Promise<CreateWalletResult> {
-    const resp = await fetch(`${this.origin}/wallet`, {
-      method: 'POST',
-      body: JSON.stringify(toDto(tx)),
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
-
-    const text = await resp.text();
-
-    let json;
-
-    try {
-      json = JSON.parse(text);
-    } catch {
-      throw new Error(`Unexpected invalid JSON response: ${text}`);
-    }
-
-    if (json === null || typeof json !== 'object' || !('failures' in json)) {
-      throw new Error(`Unexpected response: ${text}`);
-    }
-
-    return json;
-  }
 }
 
-function toDto(tx: TransactionData): TransactionDataDTO {
+function toDto(tx: Transaction): TransactionDTO {
   return {
-    ...tx,
-    nonce: tx.nonce.toHexString(),
-    ethValue: tx.ethValue.toHexString(),
+    subTransactions: tx.subTransactions.map(subTx => ({
+      publicKey: subTx.publicKey,
+      nonce: subTx.nonce.toHexString(),
+      atomic: subTx.atomic,
+      actions: subTx.actions.map(a => ({
+        ethValue: a.ethValue.toHexString(),
+        contractAddress: a.contractAddress,
+        encodedFunction: a.encodedFunction,
+      }))
+    })),
+    signature: tx.signature,
   };
 }
