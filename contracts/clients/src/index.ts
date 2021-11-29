343a6c53cf0db1752ddfcc38bb7b028b93de95b4
Andrew Morris <voltrevo@gmail.com>
2021-11-29 04:04:31 +0000

    Use import type

--- contracts/clients/src/index.ts
+++ contracts/clients/src/index.ts
@@ -1,7 +1,7 @@
 import Aggregator from "./Aggregator";
 import BlsWalletWrapper from "./BlsWalletWrapper";
 
-import { VerificationGateway } from "../typechain/VerificationGateway";
+import type { VerificationGateway } from "../typechain/VerificationGateway";
 // eslint-disable-next-line camelcase
 import { VerificationGateway__factory } from "../typechain/factories/VerificationGateway__factory";
 
