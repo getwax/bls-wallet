97e52b6a15735a6cc37dd4aa43ace0bc30fda8d6
Andrew Morris <voltrevo@gmail.com>
2022-01-11 11:50:13 +1100

    Convert WelcomeScreen to a FunctionComponent

--- extension/source/Popup/components/WelcomeScreen.tsx
+++ extension/source/Popup/components/WelcomeScreen.tsx
@@ -5,41 +5,33 @@ import Carousel from './Carousel';
 
 import LargeQuillHeading from './LargeQuillHeading';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {};
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-type State = {};
-
-export default class WelcomeScreen extends React.Component<Props, State> {
-  render(): React.ReactNode {
-    return (
-      <div className="welcome-screen">
-        <LargeQuillHeading />
-        <Carousel
-          images={[
-            browser.runtime.getURL('assets/welcome-art-1.svg'),
-            browser.runtime.getURL('assets/welcome-art-2.svg'),
-            browser.runtime.getURL('assets/welcome-art-3.svg'),
-          ]}
-        />
-        <div className="body">
-          <h3>Welcome to Quill!</h3>
-          <p>Your digital wallet for a digital world.</p>
-          <div className="get-started-container">
-            <Button
-              onPress={() => {}}
-              highlight={true}
-              icon={{
-                src: browser.runtime.getURL('assets/arrow-small.svg'),
-                px: 19,
-              }}
-            >
-              Get Started
-            </Button>
-          </div>
-        </div>
+const WelcomeScreen: React.FunctionComponent = () => (
+  <div className="welcome-screen">
+    <LargeQuillHeading />
+    <Carousel
+      images={[
+        browser.runtime.getURL('assets/welcome-art-1.svg'),
+        browser.runtime.getURL('assets/welcome-art-2.svg'),
+        browser.runtime.getURL('assets/welcome-art-3.svg'),
+      ]}
+    />
+    <div className="body">
+      <h3>Welcome to Quill!</h3>
+      <p>Your digital wallet for a digital world.</p>
+      <div className="get-started-container">
+        <Button
+          onPress={() => {}}
+          highlight={true}
+          icon={{
+            src: browser.runtime.getURL('assets/arrow-small.svg'),
+            px: 19,
+          }}
+        >
+          Get Started
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+
+export default WelcomeScreen;
