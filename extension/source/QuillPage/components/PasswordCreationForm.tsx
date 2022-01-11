5cac3e4935d12cd22eb5058b014946f48c66c0cc
Andrew Morris <voltrevo@gmail.com>
2022-01-11 16:24:42 +1100

    PasswordCreationForm

--- extension/source/QuillPage/components/PasswordCreationForm.tsx
+++ extension/source/QuillPage/components/PasswordCreationForm.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
+import QuickColumn from './QuickColumn';
+import QuickRow from './QuickRow';
 
 const PasswordCreationForm: React.FunctionComponent = () => (
   <div className="password-creation-form quick-column">
-    Password creation form
+    <QuickColumn>
+      <input
+        type="password"
+        placeholder="Password"
+        style={{ width: '100%', flexGrow: 0 }}
+      />
+      <input
+        type="password"
+        placeholder="Confirm password"
+        style={{ width: '100%', flexGrow: 0 }}
+      />
+      <QuickRow>
+        <div>Password strength</div>
+        <div>Average</div>
+      </QuickRow>
+      <div>This password is ok, but could be more secure.</div>
+    </QuickColumn>
   </div>
 );
 
