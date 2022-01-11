8ea1e9916c38dd0f192d7d852405adc833847f60
Andrew Morris <voltrevo@gmail.com>
2022-01-11 13:54:26 +1100

    Convert Carousel to a FunctionComponent

--- extension/source/Popup/components/Carousel.tsx
+++ extension/source/Popup/components/Carousel.tsx
@@ -3,34 +3,13 @@ import { Mutex } from 'async-mutex';
 
 import delay from '../../helpers/delay';
 
-type ImageState = {
-  index: number;
-  position: 'left' | 'middle' | 'right';
-};
+type Position = 'left' | 'middle' | 'right';
 
 const positionMap = {
   left: 71 - 516,
   middle: 71,
   right: 71 + 516,
 };
-
-function useGetSet<T>(initialValue: T) {
-  const [, reactSetValue] = React.useState(initialValue);
-
-  let value = initialValue;
-
-  return {
-    get: () => value,
-    set: (newValue: T) => {
-      value = newValue;
-      reactSetValue(newValue);
-    },
-    update: (partialNewValue: Partial<T>) => {
-      value = { ...value, partialNewValue };
-      reactSetValue(value);
-    },
-  };
-}
 
 const ImageSlider: React.FunctionComponent<{
   url: string;
@@ -43,7 +22,7 @@ const ImageSlider: React.FunctionComponent<{
       key="A"
       style={{
         left: `${positionMap[position]}px`,
-        ...(resetting ? { transition: 'none', visibility: 'hidden' } : {}),
+        ...(resetting ? { transition: 'none' } : {}),
       }}
     >
       <img src={url} width="258" height="215" alt="artwork" />
@@ -54,85 +33,65 @@ const ImageSlider: React.FunctionComponent<{
 const Carousel: React.FunctionComponent<{
   images: string[];
 }> = ({ images }) => {
-  const imageA = useGetSet<ImageState>({
-    index: 0,
-    position: 'middle',
-  });
+  const [imageAIndex, setImageAIndex] = React.useState(0);
+  const [imageAPosition, setImageAPosition] =
+    React.useState<Position>('middle');
 
-  const imageB = useGetSet<ImageState>({
-    index: 1,
-    position: 'right',
-  });
+  const [imageBIndex, setImageBIndex] = React.useState(1);
+  const [imageBPosition, setImageBPosition] = React.useState<Position>('right');
 
   const [resetting, setResetting] = React.useState(false);
   const [transitionMutex] = React.useState(new Mutex());
   const [intervalId, setIntervalId] = React.useState(-1);
 
-  const transitionImage = React.useCallback(
-    (index?: number) => {
-      (async () => {
-        const releaseMutex = await transitionMutex.acquire();
+  const transitionImage = async (index: number) => {
+    const releaseMutex = await transitionMutex.acquire();
 
-        const indexUpdate = { index: index ?? imageB.get().index };
-        console.log(JSON.stringify({ index, indexUpdate }));
+    setImageAPosition('left');
+    setImageBIndex(index);
+    setImageBPosition('middle');
 
-        imageA.update({ position: 'left' });
-        imageB.update({ position: 'middle', ...indexUpdate });
+    await delay(550);
 
-        await delay(550);
+    setResetting(true);
 
-        setResetting(true);
-        imageA.update({ position: 'middle', ...indexUpdate });
+    setImageAIndex(index);
+    setImageAPosition('middle');
+    setImageBPosition('right');
 
-        const newImageB: ReturnType<typeof imageB['get']> = {
-          ...imageB.get(),
-          position: 'right',
-          index: (imageB.get().index + 1) % images.length,
-        };
+    await delay(50);
 
-        imageB.update(newImageB);
+    setResetting(false);
 
-        console.log(newImageB);
-
-        await delay(50);
-
-        setResetting(false);
-
-        releaseMutex();
-      })();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [imageA, imageB],
-  );
+    releaseMutex();
+  };
 
   React.useEffect(() => {
     console.log('Running useEffect');
-    setIntervalId(setInterval(transitionImage, 5000) as unknown as number);
+    let index = 0;
+    setIntervalId(
+      setInterval(() => {
+        index = (index + 1) % images.length;
+        transitionImage(index);
+      }, 5000) as unknown as number,
+    );
 
     return () => clearInterval(intervalId);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(
-    'rendering',
-    JSON.stringify({
-      imageA: imageA.get(),
-      imageB: imageB.get(),
-    }),
-  );
-
   return (
     <div className="carousel">
       <div className="image-container">
         <ImageSlider
-          url={images[imageA.get().index]}
-          position={imageA.get().position}
+          url={images[imageAIndex]}
+          position={imageAPosition}
           resetting={resetting}
         />
         <ImageSlider
-          url={images[imageB.get().index]}
-          position={imageB.get().position}
+          url={images[imageBIndex]}
+          position={imageBPosition}
           resetting={resetting}
         />
       </div>
@@ -142,9 +101,8 @@ const Carousel: React.FunctionComponent<{
             const fillerClasses = ['radio-filler'];
 
             if (
-              (imageA.get().position === 'middle' &&
-                imageA.get().index === i) ||
-              (imageB.get().position === 'middle' && imageB.get().index === i)
+              (imageAPosition === 'middle' && imageAIndex === i) ||
+              (imageBPosition === 'middle' && imageBIndex === i)
             ) {
               fillerClasses.push('active');
             }
