a9775fcb5c41e38d7cc998b4f09b97fc5242a4e3
Andrew Morris <voltrevo@gmail.com>
2022-01-12 16:12:32 +1100

    Get pageIndex from url

--- extension/source/QuillPage/QuillPage.tsx
+++ extension/source/QuillPage/QuillPage.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 
 import OnboardingPage from './components/OnboardingPage';
 
-const QuillPage: React.FunctionComponent = () => <OnboardingPage pageIndex={0} />;
+const pageNumber = Number(
+  new URL(window.location.href).searchParams.get('p') ?? '1',
+);
+
+const QuillPage: React.FunctionComponent = () => (
+  <OnboardingPage pageIndex={pageNumber - 1} />
+);
 
 export default QuillPage;
