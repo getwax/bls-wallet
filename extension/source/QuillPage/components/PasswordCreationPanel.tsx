b5c1a0ee6bc41201bd81d52268f723f6e57ab1e1
Andrew Morris <voltrevo@gmail.com>
2022-01-14 15:49:41 +1100

    Prevent continue when password form isn't done

--- extension/source/QuillPage/components/PasswordCreationPanel.tsx
+++ extension/source/QuillPage/components/PasswordCreationPanel.tsx
@@ -5,31 +5,36 @@ import PasswordCreationForm from './PasswordCreationForm';
 
 const PasswordCreationPanel: React.FunctionComponent<{
   onComplete: () => void;
-}> = ({ onComplete }) => (
-  <>
-    <div className="instructions-text">
-      <h3>Let&apos;s start by setting a password.</h3>
-      <p>
-        Occasionally we will ask you for this to prevent unwanted access of your
-        wallets.
-      </p>
-    </div>
-    <PasswordCreationForm onPasswordUpdate={() => {}} />
-    <div>
-      <div style={{ display: 'inline-block' }}>
-        <Button
-          onPress={onComplete}
-          highlight={true}
-          icon={{
-            src: browser.runtime.getURL('assets/arrow-small.svg'),
-            px: 19,
-          }}
-        >
-          Continue
-        </Button>
+}> = ({ onComplete }) => {
+  const [password, setPassword] = React.useState<string>();
+
+  return (
+    <>
+      <div className="instructions-text">
+        <h3>Let&apos;s start by setting a password.</h3>
+        <p>
+          Occasionally we will ask you for this to prevent unwanted access of
+          your wallets.
+        </p>
       </div>
-    </div>
-  </>
-);
+      <PasswordCreationForm onPasswordUpdate={setPassword} />
+      <div>
+        <div style={{ display: 'inline-block' }}>
+          <Button
+            onPress={() => password && onComplete()}
+            highlight={true}
+            icon={{
+              src: browser.runtime.getURL('assets/arrow-small.svg'),
+              px: 19,
+            }}
+            disabled={password === undefined}
+          >
+            Continue
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default PasswordCreationPanel;
