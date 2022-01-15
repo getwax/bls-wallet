582293a0fbd58013193a1e49b9907bb52c0254a0
chaitanyapotti <chaitanya.potti@gmail.com>
2022-01-15 08:32:16 +0530

    add preferences controller

--- extension/source/Controllers/Preferences/IPreferencesController.ts
+++ extension/source/Controllers/Preferences/IPreferencesController.ts
@@ -1,8 +1,61 @@
 import { BaseConfig, BaseState, IController } from '../interfaces';
 
+export type Theme = 'light' | 'dark';
+
+export type Contact = {
+  displayName: string;
+  publicAddress: string;
+};
+
+export type CustomNft = {
+  /**
+   * Address of the nft contract
+   */
+  nftAddress: string;
+  /**
+   * Chain Id of the nft contract
+   */
+  chainId: string;
+  /**
+   * NFT standard of the nft contract. (ERC-721 or ERC-1155)
+   */
+  nftContractStandard: string;
+  /**
+   * Token Id for the nft
+   */
+  nftTokenId: string;
+};
+
+export type CustomToken = {
+  /**
+   * Address of the ERC20 token contract
+   */
+  tokenAddress: string;
+  /**
+   * Chain Id of the token contract
+   */
+  chainId: string;
+  /**
+   * Symbol of the token (e.g. 'DAI', 'USDC')
+   */
+  tokenSymbol: string;
+  /**
+   * Name of the token
+   */
+  tokenName: string;
+  /**
+   * Decimals of the token
+   */
+  decimals: string;
+};
+
 export interface AddressPreferences {
   selectedCurrency: string;
   locale: string;
+  theme: Theme;
+  contacts?: Contact[];
+  customTokens?: CustomToken[];
+  customNfts?: CustomNft[];
 }
 
 /**
@@ -23,24 +76,21 @@ export interface PreferencesState extends BaseState {
   lastSuccessMessage?: string;
 }
 
-export interface PreferencesConfig extends BaseConfig {
-  pollInterval?: number;
-}
+export type PreferencesConfig = BaseConfig;
 
-export interface IPreferencesController<P, C, S> extends IController<C, S> {
+export interface IPreferencesController<C, S> extends IController<C, S> {
   /**
-   * Init will sync the preferences of specified address with backend and also add the identity in state
-   * the store.
+   * creates a new user and stores his details
    * @param address - address of the user
    *
    */
-  initPreferences(params: { address: string; locale?: string }): Promise<void>;
+  createUser(params: { address: string } & Partial<AddressPreferences>): void;
 
   /**
    * Gets the preferences state of specified address
    * @defaultValue - By default it will return selected address preferences
    */
-  getAddressState(address?: string): P;
+  getAddressState(address?: string): AddressPreferences | undefined;
 
   /**
    * Sets the selected address in state
