4718606c60f8f630f5172a42664360df47b5844d
Andrew Morris <voltrevo@gmail.com>
2022-01-24 13:28:22 +1100

    Fix icon in password creation panel

--- extension/source/QuillPage/components/PasswordCreationPanel.tsx
+++ extension/source/QuillPage/components/PasswordCreationPanel.tsx
@@ -1,5 +1,6 @@
+import { ArrowRight } from 'phosphor-react';
 import * as React from 'react';
-import { browser } from 'webextension-polyfill-ts';
+
 import Button from '../../components/Button';
 import PasswordCreationForm from './PasswordCreationForm';
 
@@ -18,10 +19,7 @@ const PasswordCreationPanel: React.FunctionComponent = () => (
         <Button
           onPress={() => {}}
           highlight={true}
-          icon={{
-            src: browser.runtime.getURL('assets/arrow-small.svg'),
-            px: 19,
-          }}
+          icon={<ArrowRight className="icon-md" />} // TODO: Where is svg?
         >
           Continue
         </Button>
