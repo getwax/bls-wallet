a03ea2b7021bfc051d8dcfb127723672f745518a
chaitanyapotti <chaitanya.potti@gmail.com>
2022-02-02 01:13:31 +0530

    use webextension polyfill directly

--- extension/source/Popup/components/WelcomeScreen.tsx
+++ extension/source/Popup/components/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { browser } from 'webextension-polyfill-ts';
+import { runtime } from 'webextension-polyfill';
 import { ArrowRight } from 'phosphor-react';
 import Button from '../../components/Button';
 import Carousel from './Carousel';
@@ -11,9 +11,9 @@ const WelcomeScreen: React.FunctionComponent = () => (
     <LargeQuillHeading />
     <Carousel
       images={[
-        browser.runtime.getURL('assets/welcome-art-1.svg'),
-        browser.runtime.getURL('assets/welcome-art-2.svg'),
-        browser.runtime.getURL('assets/welcome-art-3.svg'),
+        runtime.getURL('assets/welcome-art-1.svg'),
+        runtime.getURL('assets/welcome-art-2.svg'),
+        runtime.getURL('assets/welcome-art-3.svg'),
       ]}
     />
     <div className="flex-grow flex flex-col gap-3 pt-2 place-items-center">
@@ -21,7 +21,7 @@ const WelcomeScreen: React.FunctionComponent = () => (
       <p className="font-light">Your digital wallet for a digital world.</p>
       <div className="mt-2">
         <Button
-          onPress={() => window.open(browser.runtime.getURL('quillPage.html'))}
+          onPress={() => window.open(runtime.getURL('quillPage.html'))}
           icon={<ArrowRight className="icon-md" />}
           className="btn-primary px-9 py-4"
         >
