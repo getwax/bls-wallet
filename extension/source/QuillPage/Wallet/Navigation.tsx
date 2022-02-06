43b7f4933de8df40bdcfbd6cc1f6b991f9561315
kautukkundan <kautukkundan@gmail.com>
2022-02-07 02:27:04 +0530

    created left NavBar added react-router-dom added quill logo with title under image created WalletPage component created NavigationBar component created routes and links

---
+++ extension/source/QuillPage/Wallet/Navigation.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import {
+  Wallet,
+  Link as LinkIcon,
+  AddressBook,
+  GearSix,
+  Lock,
+} from 'phosphor-react';
+import { Link, useLocation } from 'react-router-dom';
+import QuillHeading from '../../components/QuillHeading';
+
+const navigationTargets = [
+  {
+    name: 'Wallets',
+    icon: <Wallet className="icon-md" />,
+    target: '/',
+  },
+  {
+    name: 'Connections',
+    icon: <LinkIcon className="icon-md" />,
+    target: '/connections',
+  },
+  {
+    name: 'Contacts',
+    icon: <AddressBook className="icon-md" />,
+    target: '/contacts',
+  },
+  {
+    name: 'Settings',
+    icon: <GearSix className="icon-md" />,
+    target: '/settings',
+  },
+];
+
+export const Navigation: React.FunctionComponent = () => {
+  const { pathname } = useLocation();
+
+  const isCurrentRoute = (target: string) => {
+    return pathname === target;
+  };
+
+  return (
+    <div className="flex flex-col border-r border-grey-300 w-52 px-4 py-12">
+      <QuillHeading />
+      <div className="mt-8 flex flex-col gap-4 justify-items-center">
+        {navigationTargets.map((item) => {
+          return (
+            <Link to={item.target} key={item.name}>
+              <div
+                className={`flex gap-4 p-3 rounded-lg ${
+                  isCurrentRoute(item.target) && 'bg-grey-200'
+                }`}
+              >
+                <span
+                  className={`${
+                    isCurrentRoute(item.target) && 'text-blue-500'
+                  }`}
+                >
+                  {item.icon}
+                </span>
+                {item.name}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="flex gap-2 p-3 rounded-lg mt-20">
+        <Lock className="icon-md" />
+        Lock
+      </div>
+    </div>
+  );
+};
