5c120ccf47e109e03639f1ac6b96d4f402792d6e
Andrew Morris <voltrevo@gmail.com>
2022-02-08 06:11:35 +0000

    Expose Utilities

--- contracts/clients/src/index.ts
+++ contracts/clients/src/index.ts
@@ -4,6 +4,8 @@ import BlsWalletWrapper from "./BlsWalletWrapper";
 import type { VerificationGateway } from "../typechain/VerificationGateway";
 // eslint-disable-next-line camelcase
 import { VerificationGateway__factory } from "../typechain/factories/VerificationGateway__factory";
+import type { Utilities } from "../typechain/Utilities"; // eslint-disable-next-line camelcase
+import { Utilities__factory } from "../typechain/factories/Utilities__factory";
 import { NetworkConfig, getConfig, validateConfig } from "./NetworkConfig";
 
 export * from "./signer";
@@ -14,6 +16,9 @@ export {
   VerificationGateway,
   // eslint-disable-next-line camelcase
   VerificationGateway__factory,
+  Utilities,
+  // eslint-disable-next-line camelcase
+  Utilities__factory,
   NetworkConfig,
   getConfig,
   validateConfig,
