bb3f98e1b312222eb86ad03c0eae81a2ba66b0c1
Andrew Morris <voltrevo@gmail.com>
2022-02-09 05:45:54 +0000

    Export ERC20, MockERC20

--- contracts/clients/src/index.ts
+++ contracts/clients/src/index.ts
@@ -1,11 +1,22 @@
 import Aggregator from "./Aggregator";
 import BlsWalletWrapper from "./BlsWalletWrapper";
 
-import type { VerificationGateway } from "../typechain/VerificationGateway";
 // eslint-disable-next-line camelcase
 import { VerificationGateway__factory } from "../typechain/factories/VerificationGateway__factory";
-import type { Utilities } from "../typechain/Utilities"; // eslint-disable-next-line camelcase
+import type { VerificationGateway } from "../typechain/VerificationGateway";
+
+// eslint-disable-next-line camelcase
 import { Utilities__factory } from "../typechain/factories/Utilities__factory";
+import type { Utilities } from "../typechain/Utilities";
+
+// eslint-disable-next-line camelcase
+import { ERC20__factory } from "../typechain/factories/ERC20__factory";
+import type { ERC20 } from "../typechain/ERC20";
+
+// eslint-disable-next-line camelcase
+import { MockERC20__factory } from "../typechain/factories/MockERC20__factory";
+import type { MockERC20 } from "../typechain/MockERC20";
+
 import { NetworkConfig, getConfig, validateConfig } from "./NetworkConfig";
 
 export * from "./signer";
@@ -13,13 +24,19 @@ export * from "./signer";
 export {
   Aggregator,
   BlsWalletWrapper,
-  VerificationGateway,
-  // eslint-disable-next-line camelcase
-  VerificationGateway__factory,
-  Utilities,
-  // eslint-disable-next-line camelcase
-  Utilities__factory,
   NetworkConfig,
   getConfig,
   validateConfig,
+  // eslint-disable-next-line camelcase
+  VerificationGateway__factory,
+  VerificationGateway,
+  // eslint-disable-next-line camelcase
+  Utilities__factory,
+  Utilities,
+  // eslint-disable-next-line camelcase
+  ERC20__factory,
+  ERC20,
+  // eslint-disable-next-line camelcase
+  MockERC20__factory,
+  MockERC20,
 };
