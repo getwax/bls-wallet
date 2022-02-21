ea10069c8300182130e03f129da4377fc843fef7
kautukkundan <kautukkundan@gmail.com>
2022-02-21 21:02:58 +0530

    refactor: renamed components directory to onboarding

--- extension/source/QuillPage/components/QuickColumn.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from 'react';
-
-const QuickColumn: FunctionComponent = (props) => (
-  <div className="quick-column">{props.children}</div>
-);
-
-export default QuickColumn;
