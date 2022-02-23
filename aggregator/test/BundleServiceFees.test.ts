a75c7aeb9f3c2e7bfcbe9e3457e90dc5bdfff2d5
Andrew Morris <voltrevo@gmail.com>
2022-02-23 23:38:10 +0000

    Add failing test

---
+++ aggregator/test/BundleServiceFees.test.ts
@@ -0,0 +1,51 @@
+import { assertEquals, BigNumber } from "./deps.ts";
+import Fixture, { bundleServiceDefaultTestConfig } from "./helpers/Fixture.ts";
+
+Fixture.test("does not submit bundle with insufficient fee", async (fx) => {
+  // TODO: This test is a work in progress. It's failing for an unintended
+  // reason.
+
+  const bundleService = await fx.createBundleService({
+    ...bundleServiceDefaultTestConfig,
+    rewards: {
+      type: "ether",
+      perGas: 1_000_000_000,
+      perByte: 1_000_000_000_000, // *10?
+    },
+  });
+
+  const [wallet] = await fx.setupWallets(1);
+
+  const bundle = wallet.sign({
+    nonce: await wallet.Nonce(),
+    actions: [
+      {
+        ethValue: 0,
+        contractAddress: fx.testErc20.address,
+        encodedFunction: fx.testErc20.interface.encodeFunctionData(
+          "mint",
+          [wallet.address, 1],
+        ),
+      },
+    ],
+  });
+
+  const failures = await bundleService.add(bundle);
+  assertEquals(failures, []);
+
+  assertEquals(
+    await fx.testErc20.balanceOf(wallet.address),
+    BigNumber.from(1000),
+  );
+  assertEquals(await bundleService.bundleTable.count(), 1n);
+
+  fx.clock.advance(5000);
+  await bundleService.submissionTimer.waitForCompletedSubmissions(1);
+  await bundleService.waitForConfirmations();
+
+  assertEquals(
+    await fx.testErc20.balanceOf(wallet.address),
+    BigNumber.from(1001),
+  );
+  assertEquals(await bundleService.bundleTable.count(), 0n);
+});
