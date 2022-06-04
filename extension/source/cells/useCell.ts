f59860fbea7b6d6682a1673963c531d023195ccc
Andrew Morris <voltrevo@gmail.com>
2022-06-05 05:30:20 +1000

    Avoid useCellWithFallback

--- extension/source/cells/useCell.ts
+++ extension/source/cells/useCell.ts
@@ -3,18 +3,11 @@ import AsyncReturnType from '../types/AsyncReturnType';
 
 import { IReadableCell } from './ICell';
 
-export default function useCell<C extends IReadableCell<unknown>>(cell: C) {
-  return useCellWithFallback(cell, undefined);
-}
-
-export function useCellWithFallback<C extends IReadableCell<unknown>, F>(
-  cellParam: C,
-  initialValue: F,
-) {
+export default function useCell<C extends IReadableCell<unknown>>(cellParam: C) {
   type T = AsyncReturnType<C['read']>;
   const cell = cellParam as IReadableCell<T>;
 
-  const [value, setValue] = useState<T | F>(initialValue);
+  const [value, setValue] = useState<T>();
 
   useEffect(() => {
     let ended = false;
--- extension/source/cells/useNewCell.ts
+++ extension/source/cells/useNewCell.ts
@@ -1,11 +1,11 @@
 import { useMemo } from 'react';
 import ICell from './ICell';
 import MemoryCell from './MemoryCell';
-import { useCellWithFallback } from './useCell';
+import useCell from './useCell';
 
 export default function useNewCell<T>(initialValue: T) {
   const cell: ICell<T> = useMemo(() => new MemoryCell(initialValue), []);
-  const value = useCellWithFallback(cell, initialValue);
+  const value = useCell(cell) ?? initialValue;
 
   return [value, cell] as const;
 }
