ed6b7b77bd2ee32c55fc0c23f2c1d828c438adc1
Andrew Morris <voltrevo@gmail.com>
2022-06-05 07:17:59 +1000

    tmp

--- extension/source/cells/IAsyncStorage.ts
+++ extension/source/cells/IAsyncStorage.ts
@@ -1,8 +1,12 @@
 import * as io from 'io-ts';
+import TypedEmitter from 'typed-emitter';
 
 type IAsyncStorage = {
   read<T>(key: string, type: io.Type<T>): Promise<T | undefined>;
   write<T>(key: string, type: io.Type<T>, value: T | undefined): Promise<void>;
+  events: TypedEmitter<{
+    change(keys: string[]): void,
+  }>;
 };
 
 export default IAsyncStorage;
