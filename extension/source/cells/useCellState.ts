26fe1bfd6c95bc614d57f504b3e0efb26396de75
Andrew Morris <voltrevo@gmail.com>
2022-06-05 06:02:50 +1000

    useCellState

---
+++ extension/source/cells/useCellState.ts
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+import ICell from "./ICell";
+import MemoryCell from "./MemoryCell";
+import useCell from "./useCell";
+
+declare function useCellStateOverload<T>(
+  initialValue: T,
+): [
+  T,
+  (newValue: T) => void,
+  ICell<T>,
+];
+
+declare function useCellStateOverload<T>(
+  /* no argument given */
+): [
+  T | undefined,
+  (newValue: T | undefined) => void,
+  ICell<T | undefined>,
+];
+
+function useCellState<T>(initialValue: T): [T, (newValue: T) => void, ICell<T>] {
+  const cell = useMemo(() => new MemoryCell(initialValue), []);
+  const cellValue = useCell(cell);
+
+  return [cellValue ?? initialValue, newValue => cell.write(newValue), cell];
+}
+
+export default useCellState as typeof useCellStateOverload;
