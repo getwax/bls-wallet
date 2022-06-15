a400837b50e017c79fe1c21e9004afb794a75dd9
Andrew Morris <voltrevo@gmail.com>
2022-06-15 11:00:12 +1000

    Avoid casting window

--- extension/source/QuillPage/index.tsx
+++ extension/source/QuillPage/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
+import Browser from 'webextension-polyfill';
 
 import '../ContentScript';
 import '../styles/index.scss';
 import './styles.scss';
 import QuillPage from './QuillPage';
-import Browser from 'webextension-polyfill';
 
-(window as any).Browser ??= Browser;
+window.Browser ??= Browser;
 
 ReactDOM.render(<QuillPage />, document.getElementById('quill-page-root'));
--- extension/source/types/window.d.ts
+++ extension/source/types/window.d.ts
@@ -1,8 +1,10 @@
 import { providers } from 'ethers';
+import { Browser } from 'webextension-polyfill';
 import { QuillInPageProvider } from '../PageContentScript/InPageProvider';
 
 declare global {
   interface Window {
-    ethereum: QuillInPageProvider | providers.ExternalProvider | undefined;
+    ethereum?: QuillInPageProvider | providers.ExternalProvider;
+    Browser?: Browser;
   }
 }
