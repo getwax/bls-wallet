94cf29379f5b061abd2ad7cf8d4a215f0c56a304
Andrew Morris <voltrevo@gmail.com>
2022-06-17 15:53:48 +1000

    Handle async in MemoryCell

--- extension/source/cells/MemoryCell.ts
+++ extension/source/cells/MemoryCell.ts
@@ -5,29 +5,33 @@ import CellIterator from './CellIterator';
 import ICell, { CellEmitter } from './ICell';
 import jsonHasChanged from './jsonHasChanged';
 
-export default class MemoryCell<T> implements ICell<T> {
-  events = new EventEmitter() as CellEmitter<T>;
+export default class MemoryCell<T> implements ICell<Awaited<T>> {
+  events = new EventEmitter() as CellEmitter<Awaited<T>>;
   ended = false;
+  #valueLike: Promise<Awaited<T>>;
 
   constructor(
-    public value: T,
-    public hasChanged: ICell<T>['hasChanged'] = jsonHasChanged,
-  ) {}
-
-  async read(): Promise<T> {
-    return this.value;
+    valueLike: T,
+    public hasChanged: ICell<Awaited<T>>['hasChanged'] = jsonHasChanged,
+  ) {
+    this.#valueLike = Promise.resolve(valueLike) as Promise<Awaited<T>>;
   }
 
-  async write(newValue: T): Promise<void> {
+  async read(): Promise<Awaited<T>> {
+    return await this.#valueLike;
+  }
+
+  async write(newValueLike: T): Promise<void> {
     assert(!this.ended);
 
-    const { value: previous } = this;
-    this.value = newValue;
+    const previous = await this.#valueLike;
+    this.#valueLike = Promise.resolve(newValueLike) as Promise<Awaited<T>>;
+    const newValue = await newValueLike;
 
-    if (this.hasChanged(previous, this.value)) {
+    if (this.hasChanged(previous, newValue)) {
       this.events.emit('change', {
         previous,
-        latest: this.value,
+        latest: newValue,
       });
     }
   }
@@ -37,7 +41,7 @@ export default class MemoryCell<T> implements ICell<T> {
     this.ended = true;
   }
 
-  [Symbol.asyncIterator](): AsyncIterator<T> {
-    return new CellIterator(this);
+  [Symbol.asyncIterator](): AsyncIterator<Awaited<T>> {
+    return new CellIterator<Awaited<T>>(this);
   }
 }
