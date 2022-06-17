935bdf16c37d167989c6146e943e547d488d0f7e
Jacob Caban-Tomski <jacque006@users.noreply.github.com>
2022-06-17 16:33:08 -0400

    Add workflow for uploading extension on releases
    Add workflow for uploading extension artifacts for chrome, opera, and firefox on release.
    Add action for building and uploading extension assets.
    Remove unused extension env vars.

--- extension/source/env.ts
+++ extension/source/env.ts
@@ -2,7 +2,6 @@ import { validateConfig } from 'bls-wallet-clients';
 import { requireEnv } from './helpers/envTools';
 import toHex from './helpers/toHex';
 
-// export const ETHERSCAN_KEY = requireEnv(process.env.ETHERSCAN_KEY);
 export const PRIVATE_KEY_STORAGE_KEY = requireEnv(
   process.env.PRIVATE_KEY_STORAGE_KEY,
 );
@@ -11,8 +10,11 @@ export const AGGREGATOR_URL = requireEnv(process.env.AGGREGATOR_URL);
 export const DEFAULT_CHAIN_ID_HEX = toHex(
   parseInt(requireEnv(process.env.DEFAULT_CHAIN_ID)),
 );
-export const CREATE_TX_URL = requireEnv(process.env.CREATE_TX_URL);
 
 export const NETWORK_CONFIG = validateConfig(
   JSON.parse(requireEnv(process.env.NETWORK_CONFIG)),
+);
+
+export const CRYPTO_COMPARE_API_KEY = requireEnv(
+  process.env.CRYPTO_COMPARE_API_KEY,
 );
