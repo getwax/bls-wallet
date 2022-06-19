e3e107cbde8ffccc5c62f6ab2036ab54d5bbdfa1
Andrew Morris <voltrevo@gmail.com>
2022-06-20 07:48:50 +1000

    Add fixme

--- extension/source/cells/LongPollingCell.ts
+++ extension/source/cells/LongPollingCell.ts
@@ -2,6 +2,8 @@ import assert, { softAssert } from '../helpers/assert';
 import { IReadableCell } from './ICell';
 import MemoryCell from './MemoryCell';
 
+// FIXME: Don't longPoll actively, instead make use of a FormulaCell (or
+// otherwise) to only longPoll when needed
 export default function LongPollingCell<T>(
   longPoll: (opt?: { differentFrom: T }) => Promise<T>,
 ): IReadableCell<T> {
