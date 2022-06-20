de465645dc01ad7fc7840e3d92cca2e6f4bc5f5d
Andrew Morris <voltrevo@gmail.com>
2022-06-20 11:25:35 +1000

    Remove unused code

--- extension/source/background/utils.ts
+++ extension/source/background/utils.ts
@@ -4,9 +4,3 @@ import { encode as encode58 } from 'bs58check';
 
 export const RandomId = (): string =>
   encode58(crypto.getRandomValues(new Uint8Array(16)));
-
-export const getUserLanguage = (): string => {
-  const navLang = window.navigator.language || 'en-US';
-  const preLang = navLang.split('-');
-  return preLang[0] || 'en';
-};
