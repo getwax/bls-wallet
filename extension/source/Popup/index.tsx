68352f7384f6eb635210cb58a33980fa2d4dc924
Andrew Morris <voltrevo@gmail.com>
2022-06-21 16:02:25 +1000

    Just open the full view

--- extension/source/Popup/index.tsx
+++ extension/source/Popup/index.tsx
@@ -1,7 +1,17 @@
-import ReactDOM from 'react-dom';
+import { runtime, tabs } from 'webextension-polyfill';
 
-import './styles.scss';
-import '../styles/index.scss';
-import WelcomeScreen from './components/WelcomeScreen';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<WelcomeScreen />, document.getElementById('popup-root'));
+// import './styles.scss';
+// import '../styles/index.scss';
+// import WelcomeScreen from './components/WelcomeScreen';
+
+// ReactDOM.render(<WelcomeScreen />, document.getElementById('popup-root'));
+
+// We're not currently using the popup for anything, so just go straight to the
+// full view and close ourselves.
+tabs.create({
+  url: runtime.getURL('home.html'),
+});
+
+window.close();
