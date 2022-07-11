1c3b1c76b3b0bdaa78d75cac5cd8140b3845b5d6
Andrew Morris <voltrevo@gmail.com>
2022-07-11 18:28:44 +1000

    Require confirm dialog for Quill txs, but add todo

--- extension/source/background/AggregatorController.ts
+++ extension/source/background/AggregatorController.ts
@@ -36,10 +36,11 @@ export default class AggregatorController {
   ) {}
 
   rpc = ensureType<PartialRpcImpl>()({
-    eth_sendTransaction: async ({ providerId, params, origin }) => {
-      if (origin !== window.location.origin) {
-        await this.InternalRpc().requestTransaction(...params);
-      }
+    eth_sendTransaction: async ({ providerId, params }) => {
+      // TODO: If `origin === window.location.origin` (ie the tx is coming from
+      // inside Quill), then we should really inline the information from the
+      // dialog rather than using it.
+      await this.InternalRpc().requestTransaction(...params);
 
       // FIXME: We should not be assuming that the first from is the same as all
       // the other froms!
