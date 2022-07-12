36825dbbe6db4beb130af1a0907725d69ef2a3d6
Andrew Morris <voltrevo@gmail.com>
2022-07-12 14:39:30 +1000

    Enable linting js files, fix .eslintrc.js, tailwind.config.js

--- extension/.eslintrc.js
+++ extension/.eslintrc.js
@@ -102,29 +102,7 @@ module.exports = {
         },
       },
     ],
-    'no-restricted-syntax': [
-      'off',
-      {
-        selector: 'ForInStatement',
-        message:
-          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
-      },
-      {
-        selector: 'ForOfStatement',
-        message:
-          'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
-      },
-      {
-        selector: 'LabeledStatement',
-        message:
-          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
-      },
-      {
-        selector: 'WithStatement',
-        message:
-          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
-      },
-    ],
+    'no-restricted-syntax': 'off',
     indent: [
       'off',
       2,
