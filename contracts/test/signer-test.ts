4a5ca6bebf74b208bf980a174fb0089f451657a2
Blake Duncan <blakecduncan@gmail.com>
2022-11-14 14:11:22 +0000

    Add mock erc20 test

--- contracts/test/signer-test.ts
+++ contracts/test/signer-test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 import { parseEther } from "ethers/lib/utils";
+import { utils } from "ethers";
 
 describe("Signer test", async function () {
   let signers;
@@ -27,5 +28,34 @@ describe("Signer test", async function () {
       await signers[1].getAddress(),
     );
     expect(walletBalanceAfter.sub(walletBalanceBefore)).to.equal(ethToTransfer);
+  });
+
+  describe("ERC20", async function () {
+    let mockERC20;
+    let tokenSupply;
+    this.beforeAll(async function () {
+      tokenSupply = utils.parseUnits("1000000");
+      const MockERC20 = await ethers.getContractFactory("MockERC20");
+      mockERC20 = await MockERC20.deploy("AnyToken", "TOK", tokenSupply);
+      await mockERC20.deployed();
+      await mockERC20.transfer(await signers[0].getAddress(), tokenSupply);
+    });
+
+    it("Test sending ERC20 token from signer zero", async function () {
+      const initialBalance = await mockERC20.balanceOf(
+        await signers[1].getAddress(),
+      );
+      expect(initialBalance).to.equal(0);
+
+      // Send mockERC20 to the signer one
+      await mockERC20
+        .connect(signers[0])
+        .transfer(await signers[1].getAddress(), tokenSupply.div(2));
+
+      const newBalance = await mockERC20.balanceOf(
+        await signers[1].getAddress(),
+      );
+      expect(newBalance).to.equal(tokenSupply.div(2));
+    });
   });
 });
