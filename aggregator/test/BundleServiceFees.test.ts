30fc05cbd5aedd4d205b541822bb6ced3ca4f6fa
Andrew Morris <voltrevo@gmail.com>
2022-12-06 13:35:19 +1100

    Fix 9/10 bundle test

--- aggregator/test/BundleServiceFees.test.ts
+++ aggregator/test/BundleServiceFees.test.ts
@@ -214,18 +214,22 @@ Fixture.test("submits bundle with sufficient eth fee", async (fx) => {
 });
 
 Fixture.test("submits 9/10 bundles when 7th has insufficient fee", async (fx) => {
-  const bundleService = await createBundleService(fx);
+  const bundleService = await createBundleService(fx, {
+    type: "token",
+    address: fx.testErc20.address,
+    allowLosses: true,
+    breakevenOperationCount: 4.5,
+    ethValueInTokens: 1,
+  });
 
-  const [wallet1, wallet2] = await fx.setupWallets(2, {
+  const wallets = await fx.setupWallets(10, {
     tokenBalance: oneToken.mul(10),
   });
 
-  const nonce1 = await wallet1.Nonce();
-  const nonce2 = await wallet2.Nonce();
+  const nonce = await wallets[0].Nonce();
 
   async function addBundle(
     wallet: BlsWalletWrapper,
-    nonce: BigNumber,
     fee: BigNumber,
   ) {
     const bundle = wallet.sign(
@@ -235,22 +239,21 @@ Fixture.test("submits 9/10 bundles when 7th has insufficient fee", async (fx) =>
     assertBundleSucceeds(await bundleService.add(bundle));
   }
 
-  // 6 good bundles from wallet 1 (each pays one token)
-  await addBundle(wallet1, nonce1.add(0), oneToken);
-  await addBundle(wallet1, nonce1.add(1), oneToken);
-  await addBundle(wallet1, nonce1.add(2), oneToken);
-  await addBundle(wallet1, nonce1.add(3), oneToken);
-  await addBundle(wallet1, nonce1.add(4), oneToken);
-  await addBundle(wallet1, nonce1.add(5), oneToken);
+  // 6 good bundles
+  await addBundle(wallets[0], oneToken);
+  await addBundle(wallets[1], oneToken);
+  await addBundle(wallets[2], oneToken);
+  await addBundle(wallets[3], oneToken);
+  await addBundle(wallets[4], oneToken);
+  await addBundle(wallets[5], oneToken);
 
   // 7th bundle should fail because 1 wei is an insufficient fee
-  await addBundle(wallet1, nonce1.add(6), BigNumber.from(1));
+  await addBundle(wallets[6], BigNumber.from(1));
 
-  // 3 more good bundles. These are from a different wallet so that the nonces
-  // can be correct independent of the success/failure of bundle #7 above.
-  await addBundle(wallet2, nonce2.add(0), oneToken);
-  await addBundle(wallet2, nonce2.add(1), oneToken);
-  await addBundle(wallet2, nonce2.add(2), oneToken);
+  // 3 more good bundles
+  await addBundle(wallets[7], oneToken);
+  await addBundle(wallets[8], oneToken);
+  await addBundle(wallets[9], oneToken);
 
   assertEquals(await bundleService.bundleTable.count(), 10n);
 
@@ -260,13 +263,13 @@ Fixture.test("submits 9/10 bundles when 7th has insufficient fee", async (fx) =>
 
   assertEquals(await bundleService.bundleTable.count(), 1n);
 
-  assertEquals(
-    await fx.testErc20.balanceOf(wallet1.address),
-    oneToken.mul(4), // 6 tokens spent from wallet 1
-  );
-
-  assertEquals(
-    await fx.testErc20.balanceOf(wallet2.address),
-    oneToken.mul(7), // 3 tokens spent from wallet 2
-  );
+  await Promise.all(wallets.map((wallet, i) =>
+    (async () => {
+      assertEquals(
+        await fx.testErc20.balanceOf(wallet.address),
+        // Every wallet should have successfully spent one token, except the 7th
+        i === 6 ? oneToken.mul(10) : oneToken.mul(9),
+      );
+    })()
+  ));
 });
