cc3da2bc5d2da4cb536f8b3b295408852e5a0610
Jacob Caban-Tomski <jacque006@users.noreply.github.com>
2022-12-14 19:11:17 -0500

    Pin bls-wallet-clients@0.8.1-1995bbc

--- aggregator/deps.ts
+++ aggregator/deps.ts
@@ -50,7 +50,7 @@ export type {
   Signature,
   VerificationGateway,
   OperationResultError,
-} from "https://esm.sh/bls-wallet-clients@0.8.0-efa2e06";
+} from "https://esm.sh/bls-wallet-clients@0.8.1-1995bbc";
 
 export {
   Aggregator as AggregatorClient,
@@ -61,10 +61,10 @@ export {
   MockERC20__factory,
   VerificationGateway__factory,
   decodeError,
-} from "https://esm.sh/bls-wallet-clients@0.8.0-efa2e06";
+} from "https://esm.sh/bls-wallet-clients@0.8.1-1995bbc";
 
 // Workaround for esbuild's export-star bug
-import blsWalletClients from "https://esm.sh/bls-wallet-clients@0.8.0-efa2e06";
+import blsWalletClients from "https://esm.sh/bls-wallet-clients@0.8.1-1995bbc";
 const {
   bundleFromDto,
   bundleToDto,
