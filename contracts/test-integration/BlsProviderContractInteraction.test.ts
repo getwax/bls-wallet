d621f982d6c6f832f09ac09da3f2bd3ba2f1498b
JohnGuilding <jwguilding@gmail.com>
2023-02-17 15:45:54 +0000

    Add provider tests for base provider, event emitter, logs, inspection methods

--- contracts/test-integration/BlsProviderContractInteraction.test.ts
+++ contracts/test-integration/BlsProviderContractInteraction.test.ts
@@ -7,10 +7,12 @@ import getNetworkConfig from "../shared/helpers/getNetworkConfig";
 
 describe("Provider tests", function () {
   let blsProvider;
+  let blsSigner;
   let fundedWallet: Wallet;
 
-  this.beforeAll(async function () {
+  this.beforeAll(async () => {
     const networkConfig = await getNetworkConfig("local");
+    const privateKey = ethers.Wallet.createRandom().privateKey;
 
     const aggregatorUrl = "http://localhost:3000";
     const verificationGateway = networkConfig.addresses.verificationGateway;
@@ -25,6 +27,7 @@ describe("Provider tests", function () {
       rpcUrl,
       network,
     );
+    blsSigner = blsProvider.getSigner(privateKey);
   });
 
   describe("ERC20", async function () {
@@ -32,7 +35,7 @@ describe("Provider tests", function () {
     let tokenSupply: BigNumber;
     let recipient;
 
-    this.beforeAll(async function () {
+    this.beforeAll(async () => {
       fundedWallet = new ethers.Wallet(
         "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a", // HH Account #2 private key
         new ethers.providers.JsonRpcProvider("http://localhost:8545"),
@@ -47,15 +50,19 @@ describe("Provider tests", function () {
       );
       await mockERC20.deployed();
 
-      await mockERC20.transfer(recipient, tokenSupply);
+      await mockERC20.transfer(recipient, tokenSupply.div(2));
+      await mockERC20.transfer(
+        await blsSigner.getAddress(),
+        tokenSupply.div(2),
+      );
     });
 
-    it("balanceOf() call", async function () {
+    it("balanceOf() call", async () => {
       const balance = await mockERC20.connect(blsProvider).balanceOf(recipient);
-      expect(balance).to.equal(tokenSupply);
+      expect(balance).to.equal(tokenSupply.div(2));
     });
 
-    it("calls balanceOf successfully after instantiating Contract class with BlsProvider", async function () {
+    it("calls balanceOf successfully after instantiating Contract class with BlsProvider", async () => {
       const erc20 = new ethers.Contract(
         mockERC20.address,
         mockERC20.interface,
@@ -64,7 +71,140 @@ describe("Provider tests", function () {
       const balance = await erc20.balanceOf(recipient);
 
       expect(erc20.provider).to.equal(blsProvider);
-      expect(balance).to.equal(tokenSupply);
+      expect(balance).to.equal(tokenSupply.div(2));
+    });
+
+    it("should add event listener that is triggered by a custom filter", async () => {
+      // Arrange
+      blsProvider.removeAllListeners();
+      const erc20 = new ethers.Contract(
+        mockERC20.address,
+        mockERC20.interface,
+        blsProvider,
+      );
+
+      let value = "";
+      const setValue = async () => {
+        value = "Value set on event";
+      };
+
+      const amountToTransfer = ethers.utils.parseUnits("1");
+      const balanceBefore = await erc20.balanceOf(recipient);
+      const filter = {
+        address: mockERC20.address,
+        topics: [utils.id("Transfer(address,address,uint256)")],
+      };
+
+      // Act
+      const listenerCountBeforeEventListener =
+        blsProvider.listenerCount(filter);
+      blsProvider.on(filter, setValue); // set value when event occurs
+
+      const tx = await erc20
+        .connect(blsSigner)
+        .transfer(recipient, amountToTransfer);
+      await tx.wait();
+
+      const listenerCountDuringEventListener =
+        blsProvider.listenerCount(filter);
+
+      blsProvider.off(filter);
+
+      // Assert
+      expect((await erc20.balanceOf(recipient)).sub(balanceBefore)).to.equal(
+        amountToTransfer,
+      );
+      expect(listenerCountBeforeEventListener).to.equal(0);
+      expect(listenerCountDuringEventListener).to.equal(1);
+      expect(blsProvider.listenerCount(filter)).to.equal(0);
+      expect(value).to.equal("Value set on event");
+    });
+
+    it("should add event listener that is triggered by a custom filter and is removed automatically", async () => {
+      // Arrange
+      blsProvider.removeAllListeners();
+      const erc20 = new ethers.Contract(
+        mockERC20.address,
+        mockERC20.interface,
+        blsProvider,
+      );
+
+      let value = "";
+      const setValue = async () => {
+        value = "Value set on event";
+      };
+
+      const amountToTransfer = ethers.utils.parseUnits("1");
+      const balanceBefore = await erc20.balanceOf(recipient);
+      const filter = {
+        address: mockERC20.address,
+        topics: [utils.id("Transfer(address,address,uint256)")],
+      };
+
+      // Act
+      const listenerCountBeforeEventListener =
+        blsProvider.listenerCount(filter);
+      blsProvider.once(filter, setValue); // set value when event occurs
+      const listenerCountDuringEventListener =
+        blsProvider.listenerCount(filter);
+
+      const tx = await erc20
+        .connect(blsSigner)
+        .transfer(recipient, amountToTransfer);
+      await tx.wait();
+
+      // Assert
+      expect((await erc20.balanceOf(recipient)).sub(balanceBefore)).to.equal(
+        amountToTransfer,
+      );
+      expect(listenerCountBeforeEventListener).to.equal(0);
+      expect(listenerCountDuringEventListener).to.equal(1);
+      expect(blsProvider.listenerCount(filter)).to.equal(0);
+      expect(value).to.equal("Value set on event");
+    });
+
+    it("should return the logs for matching filters", async () => {
+      // Arrange
+      const amountToTransfer = ethers.utils.parseUnits("1");
+      const balanceBefore = await mockERC20.balanceOf(recipient);
+      const tx = await mockERC20
+        .connect(blsSigner)
+        .transfer(recipient, amountToTransfer);
+      const receipt = await tx.wait();
+
+      const transferAbi = [
+        "event Transfer(address indexed from, address indexed to, uint256 value)",
+      ];
+      const erc20Interface = new ethers.utils.Interface(transferAbi);
+
+      const expectedLogs = await blsProvider.getLogs({
+        fromBlock: "earliest",
+        toBlock: "latest",
+        address: mockERC20.address,
+        topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+      });
+
+      // Act
+      const logs = await blsProvider.getLogs({
+        fromBlock: "earliest",
+        toBlock: "latest",
+        address: mockERC20.address,
+        topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+      });
+
+      // Assert
+      const transferLog = logs.find(
+        (log) => log.transactionHash === receipt.transactionHash,
+      );
+      const transferEvent = erc20Interface.parseLog(transferLog);
+
+      expect(
+        (await mockERC20.balanceOf(recipient)).sub(balanceBefore),
+      ).to.equal(amountToTransfer);
+      expect(logs).to.deep.equal(expectedLogs);
+      expect(transferEvent.args.value).to.equal(amountToTransfer);
+      expect(transferEvent.args.from).to.equal(await blsSigner.getAddress());
+      expect(transferEvent.args.to).to.equal(recipient);
     });
   });
 });
