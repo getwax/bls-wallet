84a89cb5b6aa645814c49f2c31940ea5e45f59b4
JohnGuilding <jwguilding@gmail.com>
2023-04-19 16:10:53 +0200

    hardhat uses same chainId as geth node (1337)

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -131,6 +131,7 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
+      chainId: 1337,
       initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
       accounts,
       blockGasLimit: 30_000_000,
